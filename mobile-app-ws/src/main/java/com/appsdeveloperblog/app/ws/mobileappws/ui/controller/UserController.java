0097a5c66aae9e40486b66810848de7d4b58f0a0
teacup11 <ttea.kknezevic@gmail.com>
2021-12-06 18:25:07 +0100

    content of UserController

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,2 +1,29 @@
-package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;public class UserController {
+package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
 }
