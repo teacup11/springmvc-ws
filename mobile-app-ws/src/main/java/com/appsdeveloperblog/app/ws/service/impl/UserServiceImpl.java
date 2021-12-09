4d3303e54808eb1ea331d3f3e1d305f225390637
teacup11 <ttea.kknezevic@gmail.com>
2021-12-09 15:08:44 +0100

    adding user service implemetation class that implemets user service

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -1,2 +1,13 @@
-package com.appsdeveloperblog.app.ws.service.impl;public class UserServiceImpl {
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
 }
