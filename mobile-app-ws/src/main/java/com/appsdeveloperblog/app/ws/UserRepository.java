db154b66a52bc3c6aac7aa16b7e4388bb36ec84c
teacup11 <ttea.kknezevic@gmail.com>
2021-12-09 16:44:25 +0100

    added annotation repository

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/UserRepository.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/UserRepository.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.app.ws;
 
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 }
