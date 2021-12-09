2bf5d0f125a81072b956062f8fba06a360d2f9f2
teacup11 <ttea.kknezevic@gmail.com>
2021-12-09 17:02:36 +0100

    trying user sign up, how it works

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -1,13 +1,37 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
+import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.service.UserService;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        //for now hardcoded properties
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        //ready to save info into a database
+        UserEntity sotredUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(sotredUserDetails, returnValue);
+
+
+        return returnValue;
     }
 }
