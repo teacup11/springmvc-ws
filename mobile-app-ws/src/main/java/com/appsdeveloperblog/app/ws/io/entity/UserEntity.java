e014b6cae99855d19cba68be5e87bdaad175ae25
teacup11 <ttea.kknezevic@gmail.com>
2021-12-09 15:57:13 +0100

    set the emailVerificationStatus to false explicitly so that it is reliable and it works across different database versions and vendors

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -17,7 +17,7 @@ public class UserEntity implements Serializable {
 
 
     @Column(nullable=false)
-    private String userId;
+    private String userId; // user id which is safe to pass around the network
 
     @Column(nullable=false, length = 50)
     private String firstname;
@@ -33,8 +33,8 @@ public class UserEntity implements Serializable {
 
     private String emailVerificationToken;
 
-    @Column(nullable=false, columnDefinition = "boolean default false")
-    private Boolean emailVerificationStatus;
+    @Column(nullable=false)
+    private Boolean emailVerificationStatus = false;
 
     public long getId() {
         return id;
