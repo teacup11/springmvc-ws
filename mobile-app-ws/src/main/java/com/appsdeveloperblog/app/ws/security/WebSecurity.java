fbd793a186a4762d860e74c30e90e7b6def931f4
teacup11 <ttea.kknezevic@gmail.com>
2021-12-13 18:31:46 +0100

    added web securit extends websecurityconfigureadapter and added class userloginrequestmodel where we implement user sign-in request model

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -6,10 +6,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
-public class WebSecurity {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private final UserService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
