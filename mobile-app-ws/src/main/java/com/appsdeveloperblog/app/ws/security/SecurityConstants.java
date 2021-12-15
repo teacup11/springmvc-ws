15d78c808716fbb901e133710be655b7b077b67d
teacup11 <ttea.kknezevic@gmail.com>
2021-12-15 16:13:12 +0100

    added function that reads the token secret value

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -1,11 +1,22 @@
 package com.appsdeveloperblog.app.ws.security;
 
+import com.appsdeveloperblog.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 864000000; //10 days in milliseconds
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0"; //used in the encryption of the value access token
+
+    //token secret used in the encryption of the value access token
+    //method that reads the token secret value
+    public static String getTokenSecret() {
+
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+
+    }
+
 
 }
