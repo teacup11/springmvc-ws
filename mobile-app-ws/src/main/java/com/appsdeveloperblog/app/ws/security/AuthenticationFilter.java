c6877ad281b8a19d25386ab9c0a76d998dd1e702
teacup11 <ttea.kknezevic@gmail.com>
2021-12-15 16:15:59 +0100

    instead of access token secret call function to get token secret

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -60,7 +60,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         String token = Jwts.builder()
                 .setSubject(userName)
                 .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret())
                 .compact();
 
         // use spring application context to get user service implementation bean so that we can get user details
