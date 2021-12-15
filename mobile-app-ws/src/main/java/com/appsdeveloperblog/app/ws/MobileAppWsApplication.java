39abcebb97b417ed9f46ca78e76c6d9d931d7dec
teacup11 <ttea.kknezevic@gmail.com>
2021-12-15 16:26:44 +0100

    fixed bug - no bean named AppProperties avalible, added the needed bean name

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -24,7 +24,7 @@ public class MobileAppWsApplication {
 		return new SpringApplicationContext();
 	}
 
-	@Bean
+	@Bean(name="AppProperties")
 	public AppProperties appProperties() {
 		return new AppProperties();
 	}
