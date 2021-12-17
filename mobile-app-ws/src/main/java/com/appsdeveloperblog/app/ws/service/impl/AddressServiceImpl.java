1a3142bee9a1949ef1b66d299465564dbd868772
teacup11 <ttea.kknezevic@gmail.com>
2021-12-17 20:33:53 +0100

    Get List of Addresses Service Interface Implementation

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/AddressServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/AddressServiceImpl.java
@@ -1,13 +1,39 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
+import com.appsdeveloperblog.app.ws.io.entity.AddressEntity;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.io.repositories.UserRepository;
 import com.appsdeveloperblog.app.ws.service.AddressService;
 import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
     @Override
     public List<AddressDTO> getAddresses(String userId) {
-        return null;
+
+        List<AddressDTO> returnValue = new ArrayList<>();
+        ModelMapper modelMapper = new ModelMapper();
+
+        //query user details first
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if(userEntity == null) return returnValue;
+
+        Iterable<AddressEntity> addresses = addressRepository.findAllByUserDetails(userEntity);
+        for(AddressEntity addressEntity:addresses) {
+            returnValue.add(modelMapper.map(addressEntity, AddressDTO.class));
+        }
+
+        return returnValue;
     }
 }
