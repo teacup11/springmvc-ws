bc0de1df9dd25a291d9268fbae2708f79499f841
teacup11 <ttea.kknezevic@gmail.com>
2021-12-20 15:01:41 +0100

    added powermock-api-mockito2 dependency try fix problem with initMocks is deprecated

---
+++ mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/ui/controller/UserControllerTest.java
@@ -0,0 +1,16 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getUser() {
+    }
+}
